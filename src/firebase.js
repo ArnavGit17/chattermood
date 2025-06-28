ef69c82143d117b2055be29b0ebf6443d5ffb3ef
ArnavGit17 <arnavkumar1711@gmail.com>
2025-06-28 14:30:02 +0530

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDAlboF7iXq2mwKO7gYcXGKQ1Ix1et6PJA",
+  authDomain: "chattermood-26151.firebaseapp.com",
+  projectId: "chattermood-26151",
+  storageBucket: "chattermood-26151.firebasestorage.app",
+  messagingSenderId: "532481022826",
+  appId: "1:532481022826:web:5d8675a810fb859a5ae313"
 };
 
 const app = initializeApp(firebaseConfig);
