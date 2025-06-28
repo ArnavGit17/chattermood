748630f2910f95e474dc9d5adec517f295bf875a
ArnavGit17 <arnavkumar1711@gmail.com>
2025-06-28 23:52:31 +0530

    🎉 Initial commit: Full ChatterMood app with README and screenshots

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyByUtItymkh0Lp9vnkReB3jLJaSoKCllxc",
+  authDomain: "chattermood-realtime.firebaseapp.com",
+  projectId: "chattermood-realtime",
+  storageBucket: "chattermood-realtime.firebasestorage.app",
+  messagingSenderId: "534117355004",
+  appId: "1:534117355004:web:55124df54a4586a0743679"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
